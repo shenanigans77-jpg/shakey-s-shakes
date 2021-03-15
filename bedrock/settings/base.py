5f93ef55c1132445704f8a42edb2168fa8d360db
Stephanie Hobson <sjhobson@gmail.com>
2021-03-15 15:57:04 -0700

    port my work to pmac's simple branch

--- bedrock/settings/base.py
+++ bedrock/settings/base.py
@@ -1201,6 +1201,7 @@ CSP_IMG_SRC = CSP_DEFAULT_SRC + [
     'creativecommons.org',
     'cdn-3.convertexperiments.com',
     'logs.convertexperiments.com',
+    'images.ctfassets.net',
 ]
 CSP_SCRIPT_SRC = CSP_DEFAULT_SRC + [
     # TODO fix things so that we don't need this
